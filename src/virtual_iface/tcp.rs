fe292783921207ef452202b695e143c4ef137e58
Jonny Rimkus <jonny@rimkus.it>
2026-08-01 11:01:32 +0200

    increase tcp buffer

--- src/virtual_iface/tcp.rs
+++ src/virtual_iface/tcp.rs
@@ -19,7 +19,8 @@ use std::{
     time::Duration,
 };
 
-const MAX_PACKET: usize = 65536;
+// const MAX_PACKET: usize = 65536;
+const MAX_PACKET: usize = 262144;
 
 /// A virtual interface for proxying Layer 7 data to Layer 3 packets, and vice versa.
 pub struct TcpVirtualInterface {
